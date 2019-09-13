17db9144d384404bc62b81b74bd64ebe2e4f9791
thaiwash <30898989+thaiwash@users.noreply.github.com>
2019-09-13 11:40:25 +0300

    gitting

--- avros.js
+++ avros.js
@@ -1,3 +1,26 @@
+const { createCanvas, loadImage } = require('canvas')
+
+var canvasWidth = 500;
+var canvasHeight = 500;
+
+var window = {
+    innerWidth: canvasWidth,
+    innerHeight: canvasHeight
+
+};
+var document = {
+    createElement: function(name) {
+        if (name == "canvas") {
+            //return new Canvas(canvasWidth, canvasHeight);
+        }
+        return createCanvas(1000, 1000)
+    },
+    createElementNS: function(name) {
+
+        return createCanvas(1000, 1000)
+    }
+};
+
 var fs = require("fs")
 var THREE = require("./threejs/three.js")
 const PublicMethods = require('./PublicMethods.js')
